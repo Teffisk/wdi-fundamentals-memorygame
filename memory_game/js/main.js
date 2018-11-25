35e54c73ee8bb6fadb9c6760af6abf788574ad9f
Brad McKnight <brad.mcknight91@gmail.com>
2018-11-24 16:01:17 -0800

    created game logic and function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,38 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped a " + cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped a " + cardsInPlay[1]);
-
- if (cardsInPlay.length === 2) {
-  //console.log("you've picked two cards");
+var flipCard = function(cardId) {
+	 console.log("User flipped a " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+	//make sure 2 cards have been picked
+  if (cardsInPlay.length === 2) {
+  	//check to see if the cards match
 	if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match"); 
 	}
 	else { 
+		//alert if the cards don't match
 		alert("Sorry, try again");
 	}
-}
+ 
+  }
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
