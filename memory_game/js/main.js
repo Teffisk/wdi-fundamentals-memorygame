6e73760841521f6ceabcdf50bdec76551bfe081d
Brad McKnight <brad.mcknight91@gmail.com>
2018-11-25 17:17:54 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,28 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var flipCard = function(cardId) {
-	 console.log("User flipped a " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+	 console.log("User flipped a " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	//make sure 2 cards have been picked
   if (cardsInPlay.length === 2) {
   	//check to see if the cards match
@@ -14,25 +33,8 @@ var flipCard = function(cardId) {
 		//alert if the cards don't match
 		alert("Sorry, try again");
 	}
- 
   }
 };
 
 flipCard(0);
 flipCard(2);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
